67b7f0c31c55bff554a575fa509ea6a7e564e538
Scribble <scribble@minecrafttas.com>
2025-09-13 19:40:34 +0200

    Update KTRNGSeedStorage to new format

--- src/main/java/com/minecrafttas/tasmod/savestates/storage/builtin/KTRNGSeedStorage.java
+++ src/main/java/com/minecrafttas/tasmod/savestates/storage/builtin/KTRNGSeedStorage.java
@@ -1,67 +1,32 @@
 package com.minecrafttas.tasmod.savestates.storage.builtin;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.minecrafttas.tasmod.TASmod;
-import com.minecrafttas.tasmod.savestates.SavestateHandlerServer;
-import com.minecrafttas.tasmod.savestates.exceptions.LoadstateException;
-import com.minecrafttas.tasmod.savestates.exceptions.SavestateException;
-import com.minecrafttas.tasmod.savestates.storage.AbstractExtendStorage;
+import com.minecrafttas.tasmod.savestates.storage.SavestateStorageExtensionBase;
 
 import net.minecraft.server.MinecraftServer;
 
-public class KTRNGSeedStorage extends AbstractExtendStorage {
+public class KTRNGSeedStorage extends SavestateStorageExtensionBase {
 
-	private static final Path fileName = Paths.get("globalSeed.json");
-	private final Gson json = new GsonBuilder().setPrettyPrinting().create();
+	public KTRNGSeedStorage() {
+		super("killtherngSeeds.json");
+	}
 
 	@Override
-	public void onServerSavestate(MinecraftServer server, int index, Path target, Path current) {
+	public JsonObject onSavestate(MinecraftServer server, JsonObject dataToSave) {
 		long currentSeed = TASmod.globalRandomness.getCurrentSeed();
-		JsonObject seedObject = new JsonObject();
-		seedObject.addProperty("globalSeed", currentSeed);
-		saveJson(current, seedObject);
-	}
-
-	private void saveJson(Path current, JsonObject data) {
-		Path saveFile = current.resolve(SavestateHandlerServer.storageDir).resolve(fileName);
-
-		String out = json.toJson(data);
-
-		try {
-			Files.write(saveFile, out.getBytes());
-		} catch (IOException e) {
-			throw new SavestateException(e, "Could not write to the file system");
-		}
+		dataToSave.addProperty("globalSeed", currentSeed);
+		return dataToSave;
 	}
 
 	@Override
-	public void onServerLoadstate(MinecraftServer server, int index, Path target, Path current) {
-		JsonObject seedObject = loadJson(target);
-		if (!seedObject.has("globalSeed"))
-			return;
-		long newSeed = seedObject.get("globalSeed").getAsLong();
+	public void onLoadstate(MinecraftServer server, JsonObject loadedData) {
+		long newSeed = loadedData.get("globalSeed").getAsLong();
 		TASmod.globalRandomness.setSeed(newSeed);
 	}
 
-	private JsonObject loadJson(Path target) {
-		Path saveFile = target.resolve(SavestateHandlerServer.storageDir).resolve(fileName);
-
-		if (!Files.exists(saveFile))
-			return new JsonObject();
-
-		String in;
-		try {
-			in = new String(Files.readAllBytes(saveFile));
-		} catch (IOException e) {
-			throw new LoadstateException(e, "Could not read from the file system");
-		}
-		return json.fromJson(in, JsonObject.class);
+	@Override
+	public String getExtensionName() {
+		return "KTRNGSeedStorage";
 	}
 }
