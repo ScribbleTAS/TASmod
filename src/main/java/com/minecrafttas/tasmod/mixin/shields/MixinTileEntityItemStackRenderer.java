f1009f043e6c756697cddfb865f258ef609957c7
Scribble <scribble@minecrafttas.com>
2026-03-27 09:00:44 +0100

    Fix crash when opening an inventory

--- src/main/java/com/minecrafttas/tasmod/mixin/shields/MixinTileEntityItemStackRenderer.java
+++ src/main/java/com/minecrafttas/tasmod/mixin/shields/MixinTileEntityItemStackRenderer.java
@@ -16,7 +16,7 @@ import net.minecraft.util.ResourceLocation;
 @Mixin(TileEntityItemStackRenderer.class)
 public class MixinTileEntityItemStackRenderer {
 
-	@Redirect(method = "Lnet/minecraft/client/renderer/tileentity/TileEntityItemStackRenderer;renderByItem(Lnet/minecraft/item/ItemStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureManager;bindTexture(Lnet/minecraft/util/ResourceLocation;)V", ordinal = 1))
+	@Redirect(method = "renderByItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureManager;bindTexture(Lnet/minecraft/util/ResourceLocation;)V", ordinal = 1))
 	public void inject_renderByItem(TextureManager manager, ResourceLocation original, ItemStack shield) {
 		EntityLivingBase entity = ShieldDownloader.renderedEntity;
 		if (entity != null && shield != null) {
