1271e70e877ae35af3b148bec6175e161ff5b14b
Scribble <scribble@minecrafttas.com>
2026-05-25 23:22:55 +0200

    [KillTheRNG] Make loadedEntityList a SortedList

--- src/main/java/com/minecrafttas/tasmod/mixin/killtherng/MixinWorld.java
+++ src/main/java/com/minecrafttas/tasmod/mixin/killtherng/MixinWorld.java
@@ -1,6 +1,8 @@
 package com.minecrafttas.tasmod.mixin.killtherng;
 
+import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +10,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.minecrafttas.tasmod.TASmod;
 import com.minecrafttas.tasmod.ktrng.builtin.WorldRNG;
+import com.minecrafttas.tasmod.util.SortedList;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
@@ -33,5 +39,20 @@ public class MixinWorld {
 	@ModifyReturnValue(method = "setRandomSeed", at = @At(value = "RETURN"))
 	public Random modify_worldSetRNGReturn(Random original) {
 		return TASmod.worldSeedRandomness;
+	}
+
+	@WrapOperation(method = "<init>", at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;loadedEntityList:Ljava/util/List;"))
+	private <E> void modify_loadedEntityList(World owner, List<E> list, Operation<Void> operation) {
+		operation.call(owner, new SortedList<Entity>((entity, entity2) -> {
+			if (entity == null || entity2 == null)
+				return 0;
+
+			UUID uuid = entity.getUniqueID();
+			UUID uuid2 = entity.getUniqueID();
+
+			if (uuid == null || uuid2 == null)
+				return 0;
+			return uuid.toString().compareTo(uuid2.toString());
+		}));
 	}
 }
